c9f2fce7076fe136483b358d4a755de41ebc33da
Paul Rodriguez <prodr010@ucr.edu>
2019-06-12 18:42:30 -0700

    MAC results

---
+++ test_ping_google.py
@@ -0,0 +1,41 @@
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+import scipy.linalg as la
+
+print("loading WiFi series... ")
+
+wifi_bloated = np.loadtxt('wifi_bloated_google.txt',delimiter=' ',dtype=float)
+wifi_clean = np.loadtxt('wifi_clean_google.txt',delimiter=' ',dtype=float)
+# lte_bloated = np.loadtxt('lte_bloated_google.txt',delimiter=' ',dtype=float)
+# lte_clean = np.loadtxt('lte_clean_google.txt',delimiter=' ',dtype=float)
+
+time_w_b = wifi_bloated[:,0]
+ping_w_b = wifi_bloated[:,1]
+
+time_w_c = wifi_clean[:,0]
+ping_w_c = wifi_clean[:,1]
+
+# time_l_b = lte_bloated[:,0]
+# ping_l_b = lte_bloated[:,1]
+
+# time_l_c = lte_clean[:,0]
+# ping_l_c = lte_clean[:,1]
+
+plt.plot(time_w_c,ping_w_c,'r-', label='WiFi clean')
+plt.plot(time_w_b,ping_w_b,'m-', label='WiFi with bloat')
+# plt.plot(time_l_c,ping_l_c,'b--', label='LTE clean')
+# plt.plot(time_l_b,ping_l_b,'c--', label='LTE with bloat')
+
+plt.xticks(np.arange(0,49,step=2))
+plt.grid(True)
+plt.legend()
+plt.xlim(0,49)
+# plt.ylim(0,400)
+plt.ylim(0,55)
+plt.xlabel('time (s)')
+plt.ylabel('ping time (ms)')
+plt.title('WiFi Results (www.google.com)')
+# plt.title('LTE Results (www.google.com)')
+# plt.title('WiFi vs LTE Results (www.google.com)')
+plt.show()
---
+++ test_traceroute.py
@@ -0,0 +1,52 @@
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+import scipy.linalg as la
+
+print("loading google traceroute... ")
+
+wifi_traceroute_google = np.loadtxt('traceroute_wifi_google.txt',delimiter=' ',dtype=float)
+lte_traceroute_google = np.loadtxt('traceroute_lte_google.txt',delimiter=' ',dtype=float)
+
+hop_w_google = wifi_traceroute_google[:,0]
+time_w_google = wifi_traceroute_google[:,1:4]
+avg_w_google = np.average(time_w_google,axis=1)
+
+hop_l_google = lte_traceroute_google[:,0]
+time_l_google = lte_traceroute_google[:,1:4]
+avg_l_google = np.average(time_l_google,axis=1)
+
+plt.plot(hop_w_google,avg_w_google,'r-', label='WiFi')
+plt.plot(hop_l_google,avg_l_google,'c--', label='LTE')
+
+# print("loading g5 traceroute... ")
+
+# wifi_traceroute_g5 = np.loadtxt('traceroute_wifi_g5.txt',delimiter=' ',dtype=float)
+# lte_traceroute_g5 = np.loadtxt('traceroute_lte_g5.txt',delimiter=' ',dtype=float)
+
+# hop_w_g5 = wifi_traceroute_g5[:,0]
+# time_w_g5 = wifi_traceroute_g5[:,1:]
+# avg_w_g5 = np.average(time_w_g5,axis=1)
+
+# hop_l_g5 = lte_traceroute_g5[:,0]
+# time_l_g5 = lte_traceroute_g5[:,1:4]
+# avg_l_g5 = np.average(time_l_g5,axis=1)
+
+# plt.plot(hop_w_g5,avg_w_g5,'r-', label='WiFi')
+# plt.plot(hop_l_g5,avg_l_g5,'c--', label='LTE')
+
+plt.grid(True)
+plt.legend()
+plt.xlabel('hop')
+plt.ylabel('rtt (ms)')
+
+plt.xlim(1,11)
+plt.ylim(0,60)
+plt.xticks(np.arange(1,12,step=1))
+plt.title('WiFi vs LTE Traceroute (www.google.com)')
+
+# plt.xlim(1,20)
+# plt.ylim(0,450)
+# plt.xticks(np.arange(1,21,step=1))
+# plt.title('WiFi vs LTE Traceroute (www.g5web.com)')
+plt.show()
